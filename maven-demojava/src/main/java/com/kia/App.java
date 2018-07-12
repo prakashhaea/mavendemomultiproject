677322f2251807f8570701837f79add42202dea0
Prakash <ppalaniyappan@autoeveramerica.com>
2018-07-11 17:06:12 -0700

    committed changes

--- maven-demojava/src/main/java/com/kia/App.java
+++ maven-demojava/src/main/java/com/kia/App.java
@@ -1,13 +1,11 @@
 package com.kia;
-
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App{
+	public static void main( String[] args ){
         System.out.println( "Hello World!!" );
-    }
+        
+       	}
 }
